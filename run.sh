c948fa7e7ea1b0c57b0c77b42a73d7deb72d58d1
Vitor Freitas <vitorldfreitas@gmail.com>
2020-04-21 14:45:10 -0300

    add run script to create symlinks

---
+++ run.sh
@@ -0,0 +1,6 @@
+# Create symlinks
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/nvim/plugins.vim ~/.config/nvim/plugins.vim
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/i3/config ~/.i3/config
